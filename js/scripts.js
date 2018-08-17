e670e87580232af0fef6d34c8b08eac414f54f4f
Nikki Wong <nikkiwong@miami.edu>
2018-08-17 16:21:18 -0700

    modify js so user name appears in message

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 var beep = "Beep!";
 var boop = "Boop!";
-var spaceOdysseyQuote = "I'm sorry, Dave. I'm afraid I can't do that.";
-var spaceOdysseyQuestion = "Just what do you think you're doing, Dave?";
 var errorMessage = "Please enter a valid number.";
+var inputtedName;
 
 var convertNumbers = function(inputtedNumber) {
   var displayedArray = [];
@@ -14,9 +13,9 @@ var convertNumbers = function(inputtedNumber) {
 
   for (var i = 0; i <= inputtedNumber; i++) {
     if (inputtedNumber % 3 === 0 && inputtedNumber != 0) {
-      var displayMessage = spaceOdysseyQuote;
+      var displayMessage = "I'm sorry, " + inputtedName + ". I'm afraid I can't do that.";
     } else if (inputtedNumber % 7 === 0 && inputtedNumber != 0) {
-      var displayMessage = spaceOdysseyQuestion;
+      var displayMessage = "Just what do you think you're doing, " + inputtedName + "?";
     } else if (inputtedNumberString.includes(1)) {
       var displayMessage = boop;
     } else if (inputtedNumberString.includes(0)) {
@@ -36,6 +35,7 @@ var convertNumbers = function(inputtedNumber) {
 $(document).ready(function() {
   $("form#input-form").submit(function(event) {
     event.preventDefault();
+    inputtedName = $("input#nameInput").val();
     var inputtedNumber = parseInt($("input#userInput").val());
 
     var outputtedMessage = convertNumbers(inputtedNumber);
