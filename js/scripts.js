710921097cf2007c6f1b1e512af8cd683061de99
Nikki Wong <nikkiwong@miami.edu>
2018-08-17 10:24:19 -0700

    add final condition of values divisible by 3 returning space odyssey quote

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-//business logic
 var beep = "Beep!";
 var boop = "Boop!";
 var spaceOdysseyQuote = "I'm sorry, Dave. I'm afraid I can't do that."
@@ -8,10 +7,11 @@ var convertNumbers = function(inputtedNumber) {
   var inputtedNumberString = inputtedNumber.toString();
 
   for (var i = 1; i <= inputtedNumber; i++) {
-    if (inputtedNumberString.includes(1)) {
+    if (inputtedNumber % 3 === 0) {
+      var displayMessage = spaceOdysseyQuote;
+    } else if (inputtedNumberString.includes(1)) {
       var displayMessage = boop;
-    }
-    else if (inputtedNumberString.includes(0)) {
+    } else if (inputtedNumberString.includes(0)) {
       var displayMessage = beep;
     } else {
       displayedArray.push(i);
@@ -19,18 +19,16 @@ var convertNumbers = function(inputtedNumber) {
     }
   }
 
-
   return displayMessage;
 };
 
-// user interface logic
 $(document).ready(function() {
   $("form#input-form").submit(function(event) {
     event.preventDefault();
     var inputtedNumber = parseInt($("input#userInput").val());
 
     var outputtedMessage = convertNumbers(inputtedNumber);
-    $("#text-here").text(outputtedMessage);
+    $("#messageToUser").text(outputtedMessage);
     $("#result").show();
   });
 });
